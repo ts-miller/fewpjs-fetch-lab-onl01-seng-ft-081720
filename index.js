0d755474157e87735699f9ee2a801040b7699563
ts-miller <tylerstephen11@gmail.com>
2020-11-23 15:27:45 -0500

    commit tree 69550dedf2214f14373d494d0b1b265ef2573bc2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(function(json) {
+      renderBooks(json)
+    })
 }
 
 function renderBooks(books) {
